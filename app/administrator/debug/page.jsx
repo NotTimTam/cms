feb9750626b794afe7c9de3b988787440216a102
NotTimTam <timothyrp6@gmail.com>
2024-11-01 20:59:43 -0700

    Added administrator debug page.

---
+++ app/administrator/debug/page.jsx
@@ -0,0 +1,16 @@
+export const metadata = {
+	title: "Debugging",
+};
+
+export default function Debug() {
+	return (
+		<>
+			<h1>Debug</h1>
+			<p>
+				This page exists for debugging purposes. Interacting with it
+				blindly could introduce unexpected results, bugs, or crashes
+				with your instance of the cms.
+			</p>
+		</>
+	);
+}

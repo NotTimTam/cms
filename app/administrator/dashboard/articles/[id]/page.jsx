788ed142cb98962813b1bd8acdd3df18d6b90a52
NotTimTam <timothyrp6@gmail.com>
2024-11-03 22:06:24 -0700

    Tracked some untracked files.

---
+++ app/administrator/dashboard/articles/[id]/page.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+	title: "Edit Article",
+};
+
+export default function Article() {
+	return <>Article (JUST ONE)</>;
+}
---
+++ app/administrator/dashboard/articles/page.jsx
@@ -0,0 +1,14 @@
+import Filter from "../../components/Filter";
+
+export const metadata = {
+	title: "Articles",
+};
+
+export default function Articles() {
+	return (
+		<>
+			<Filter />
+			List articles here...
+		</>
+	);
+}

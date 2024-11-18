5e402934a88df99abd3c3e4b20b28af5cfe643c1
NotTimTam <timothyrp6@gmail.com>
2024-11-18 12:38:32 -0700

    Tracked untracked files.

---
+++ server/config/constructors.js
@@ -0,0 +1,57 @@
+import { log, success, warn } from "@nottimtam/console.js";
+import bcrypt from "bcryptjs";
+import UserRoleModel from "../models/users/UserRole.js";
+import UserModel from "../models/users/User.js";
+import { generateRandomPassword } from "../../util/data.js";
+
+/**
+ * Ensure that a webmaster exists, and create one if it does not.
+ */
+export const constructWebmaster = async () => {
+	log("Checking for Webmaster...");
+
+	// Role
+	let webmasterRole = await UserRoleModel.findOne({ name: "Webmaster" });
+
+	if (!webmasterRole) {
+		webmasterRole = new UserRoleModel({
+			name: "Webmaster",
+			description:
+				"System-generated user role, reserved to the webmaster user.",
+			locked: true,
+		});
+
+		await webmasterRole.save();
+
+		success("Created Webmaster user role.");
+	} else if (!webmasterRole.locked) {
+		webmasterRole.locked = true;
+		await webmasterRole.save();
+
+		warn(
+			"Webmaster role was left unlocked, (editable) but has been relocked. While this is generally not a concern, the Webmaster role can only be unlocked through direct database manipulation. If you did not make this change, please verify your system is secure or contact your deployment's webmaster."
+		);
+	}
+
+	// User
+	let user = await UserModel.findOne({
+		roles: { $in: [webmasterRole._id] },
+	});
+
+	if (!user) {
+		const password = generateRandomPassword(12);
+		user = new UserModel({
+			name: "Webmaster",
+			username: "webmaster",
+			password: await bcrypt.hash(password, +process.env.SALT || 12),
+			verified: false,
+			roles: [webmasterRole._id],
+		});
+
+		await user.save();
+
+		warn(
+			`No webmaster user exists. One has been created with the credentials:\n\nUsername: ${user.username}\nPassword: ${password}\n\nLogin as this webmaster and change the password immediately.`
+		);
+	} else success("Webmaster exists.");
+};
